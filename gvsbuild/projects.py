cc5182c1940118d5168d90148b6ffbfff036f1b9
Chungrae Cho <hsccr@amk.ne.kr>
2017-05-22 11:27:46 +0900

    build/libuv: apply patch using python 2.7
    파이썬 2.7 버젼을 사용하도록 패치를 적용했습니다.

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -938,6 +938,7 @@ class Project_libuv(Tarball, Project):
             'libuv',
             archive_url = 'https://github.com/libuv/libuv/archive/v1.9.1.tar.gz',
             hash = 'a6ca9f0648973d1463f46b495ce546ddcbe7cce2f04b32e802a15539e46c57ad',
+            patches = ['001-using-python27.patch'],
             )
 
     def build(self):
